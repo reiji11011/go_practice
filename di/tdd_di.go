14b0830e52acde1d9bc305ce771456ee200315dc
reiji11011 <reiji.sugama@gmail.com>
2022-09-26 00:00:14 +0900

    [add]

--- di/tdd_di.go
+++ di/tdd_di.go
@@ -1,12 +1,26 @@
 package di
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // 参考：スタブ・モック - テスト駆動開発でGO言語を学びましょう
 // https://andmorefine.gitbook.io/learn-go-with-tests/go-fundamentals/mocking
 
 func TddDi() {
-	CountDown()
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "太郎")
 }
 
 func CountDown() {
-	
+
+}
+
+// 以下のテストを書く。
+// fmt.Printf()を実行するとstdoutに出力される。
+// 依存関係を注入できるようにする。
+
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
 }
